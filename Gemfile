2817a79a51db4c4e305467dbc743e13acd7ec1d7
elazar <tobias382@gmail.com>
2020-05-25 08:33:30 -0500

    Consolidate scripts, fix cropping issue

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 gem 'mechanize'
 gem 'prawn'
+
+gem "image_size", "~> 2.0"
--- issuu_downloader.rb
+++ issuu_downloader.rb
@@ -1,56 +1,56 @@
 #!/usr/bin/ruby
+require 'image_size'
 require 'mechanize'
 require 'prawn'
-=begin
-running from terminal to download any pdf from issuu.com, given
-- magazine name
-- page count
-- document-id
-=end
 
-prompt = "> "
+$stdout.sync = true
 
-puts "What is the name of the magazine you'd like to download from issuu.com? ps: this will be the name of your pdf file\n"
-print prompt
-magazine_name = gets.chomp
+document_id = ARGV[0].to_s
+magazine_name = ARGV[1].to_s
 
+agent = Mechanize.new
+pages = 0
+more = true
 
-puts "How many pages does it have?\n ie 104\n"
-print prompt
-page_number = gets.chomp
-
-puts "document Id? \n to get the 'document-id' inspect page in chrome,\n search for document-id and paste here,\n ie 140601160255-3a4c0f75ec731801ef369f5000f03104\n"
-print prompt
-document_id = gets.chomp
-
-for i in 1..page_number.to_i
-  print "downloading\tpage n #{i}\n"
-  agent = Mechanize.new
-  link = "http://image.issuu.com/#{document_id.to_s}/jpg/page_#{i.to_s}.jpg"
-  agent.get(link).save "page_#{i.to_s}.jpg"
-  print "downloaded\tpage n #{i}\n"
+print "Downloading "
+while more
+  print "."
+  page = pages + 1
+  filename = "page_#{page.to_s}.jpg"
+  link = "http://image.issuu.com/#{document_id}/jpg/#{filename}"
+  begin
+    image = agent.get(link)
+    image.save filename
+    pages = page
+  rescue Mechanize::ResponseCodeError => e
+    more = e.response_code == "404"
+  end
 end
+puts
 
-print "images from 1 to #{page_number.to_s} downloaded as jpg\n"
-
-########to combine all images into a pdf
-
+print "Generating "
 Prawn::Document.generate("#{magazine_name}.pdf", :page_layout => :portrait) do |pdf|
-
-  for i in 1..page_number.to_i
-      pdf.image "page_#{i.to_s}.jpg", :at => [0,750], :width => 530
-      pdf.start_new_page
-  end#end of loop
+  for page in 1..pages
+    print "."
+    filename = "page_#{page.to_s}.jpg"
+    size = ImageSize.path(filename)
+    pdf.image filename, {
+      :width => size.width,
+      :height => size.height,
+      :position => :center,
+      :vposition => :center,
+      :fit => [pdf.bounds.right.to_i, pdf.bounds.top.to_i]
+    }
+    pdf.start_new_page if page != pages
+  end
 end
+puts
 
-print "images from 1 to #{page_number.to_s} combined into pdf \n"
+print "Cleaning up "
+for page in 1..pages
+  print "."
+  File.delete("page_#{page.to_s}.jpg")
+end
+puts
 
-########to delete all images, once pdf as been created, to clean up a bit
-for i in 1..page_number.to_i
-  File.delete("page_#{i.to_s}.jpg")
-end#end of prawn
-
-print "images from 1 to #{page_number.to_s} deleted \n"
-# print "your pdf #{magazine_name}.pdf is in: \n #{Dir.pwd}"
-
-
+puts "Done"
